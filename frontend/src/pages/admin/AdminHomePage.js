c0bf5bed28e6a3933b1f0b51d70ab5dc4cc1cae1
devid-koch <devidkoch33@gmail.com>
2024-02-09 22:49:02 +0530

    fixes

--- frontend/src/pages/admin/AdminHomePage.js
+++ frontend/src/pages/admin/AdminHomePage.js
@@ -1,16 +1,16 @@
-import { Container, Grid, Paper } from '@mui/material'
-import SeeNotice from '../../components/SeeNotice';
+import { Container, Grid, Paper } from "@mui/material";
+import SeeNotice from "../../components/SeeNotice";
 import Students from "../../assets/img1.png";
 import Classes from "../../assets/img2.png";
 import Teachers from "../../assets/img3.png";
 import Fees from "../../assets/img4.png";
-import styled from 'styled-components';
-import CountUp from 'react-countup';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getAllSclasses } from '../../redux/sclassRelated/sclassHandle';
-import { getAllStudents } from '../../redux/studentRelated/studentHandle';
-import { getAllTeachers } from '../../redux/teacherRelated/teacherHandle';
+import styled from "styled-components";
+import CountUp from "react-countup";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllSclasses } from "../../redux/sclassRelated/sclassHandle";
+import { getAllStudents } from "../../redux/studentRelated/studentHandle";
+import { getAllTeachers } from "../../redux/teacherRelated/teacherHandle";
 
 const AdminHomePage = () => {
     const dispatch = useDispatch();
@@ -18,9 +18,9 @@ const AdminHomePage = () => {
     const { sclassesList } = useSelector((state) => state.sclass);
     const { teachersList } = useSelector((state) => state.teacher);
 
-    const { currentUser } = useSelector(state => state.user)
+    const { currentUser } = useSelector((state) => state.user);
 
-    const adminID = currentUser._id
+    const adminID = currentUser._id;
 
     useEffect(() => {
         dispatch(getAllStudents(adminID));
@@ -35,44 +35,65 @@ const AdminHomePage = () => {
     return (
         <>
             <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                <Grid container spacing={3}>
+                <Grid
+                    container
+                    spacing={3}
+                    flex={"flex"}
+                    justifyContent={"center"}
+                >
                     <Grid item xs={12} md={3} lg={3}>
                         <StyledPaper>
                             <img src={Students} alt="Students" />
-                            <Title>
-                                Total Students
-                            </Title>
-                            <Data start={0} end={numberOfStudents} duration={2.5} />
+                            <Title>Total Students</Title>
+                            <Data
+                                start={0}
+                                end={numberOfStudents}
+                                duration={2.5}
+                            />
                         </StyledPaper>
                     </Grid>
                     <Grid item xs={12} md={3} lg={3}>
                         <StyledPaper>
                             <img src={Classes} alt="Classes" />
-                            <Title>
-                                Total Classes
-                            </Title>
-                            <Data start={0} end={numberOfClasses} duration={5} />
+                            <Title>Total Classes</Title>
+                            <Data
+                                start={0}
+                                end={numberOfClasses}
+                                duration={5}
+                            />
                         </StyledPaper>
                     </Grid>
                     <Grid item xs={12} md={3} lg={3}>
                         <StyledPaper>
                             <img src={Teachers} alt="Teachers" />
-                            <Title>
-                                Total Teachers
-                            </Title>
-                            <Data start={0} end={numberOfTeachers} duration={2.5} />
+                            <Title>Total Teachers</Title>
+                            <Data
+                                start={0}
+                                end={numberOfTeachers}
+                                duration={2.5}
+                            />
                         </StyledPaper>
                     </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
+                    {/* <Grid item xs={12} md={3} lg={3}>
                         <StyledPaper>
                             <img src={Fees} alt="Fees" />
-                            <Title>
-                                Fees Collection
-                            </Title>
-                            <Data start={0} end={23000} duration={2.5} prefix="$" />                        </StyledPaper>
-                    </Grid>
+                            <Title>Per Semester Fees</Title>
+                            <Data
+                                start={0}
+                                end={8000}
+                                duration={2.5}
+                                prefix="$"
+                            />{" "}
+                        </StyledPaper>
+                    </Grid> */}
                     <Grid item xs={12} md={12} lg={12}>
-                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: "flex",
+                                flexDirection: "column",
+                            }}
+                        >
                             <SeeNotice />
                         </Paper>
                     </Grid>
@@ -82,24 +103,23 @@ const AdminHomePage = () => {
     );
 };
 
-
 const StyledPaper = styled(Paper)`
-  padding: 16px;
-  display: flex;
-  flex-direction: column;
-  height: 200px;
-  justify-content: space-between;
-  align-items: center;
-  text-align: center;
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    height: 200px;
+    justify-content: space-between;
+    align-items: center;
+    text-align: center;
 `;
 
 const Title = styled.p`
-  font-size: 1.25rem;
+    font-size: 1.25rem;
 `;
 
 const Data = styled(CountUp)`
-  font-size: calc(1.3rem + .6vw);
-  color: green;
+    font-size: calc(1.3rem + 0.6vw);
+    color: green;
 `;
 
-export default AdminHomePage
+export default AdminHomePage;
